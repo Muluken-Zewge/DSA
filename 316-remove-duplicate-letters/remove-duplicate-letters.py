f171e109509910691930d2d7b181cfb73b981383
Muluken Zewge <mulukenzewge@gmail.com>
2026-01-01 15:22:31 +0300

    Time: 5 ms (15.93%) | Memory: 17.6 MB (82.86%) - LeetSync

--- 316-remove-duplicate-letters/remove-duplicate-letters.py
+++ 316-remove-duplicate-letters/remove-duplicate-letters.py
@@ -1,15 +1,19 @@
 class Solution:
     def removeDuplicateLetters(self, s: str) -> str:
         stack = []
-        count = Counter(s)
+        # to check if char exists to the right of the current char
+        count = Counter(s) 
+        seen = set() # to check duplicate
 
         for char in s:
             count[char] -= 1
-            if char in stack:
+            if char in seen:
                 continue
-
+            # if current char is smaller than chars in the stack and we can find them later, pop them out
             while stack and char < stack[-1] and count[stack[-1]] > 0:
-                stack.pop()
+                seen.remove(stack.pop())
+
             stack.append(char)
+            seen.add(char)
 
         return "".join(stack)
