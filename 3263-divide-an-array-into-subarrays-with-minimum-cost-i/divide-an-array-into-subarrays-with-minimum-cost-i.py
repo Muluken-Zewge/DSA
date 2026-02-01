627f5b4f34208260d4a575192a750489865d4b75
Muluken Zewge <mulukenzewge@gmail.com>
2026-02-01 19:18:40 +0300

    Time: 3 ms (35.86%) | Memory: 19.4 MB (32.83%) - LeetSync

--- 3263-divide-an-array-into-subarrays-with-minimum-cost-i/divide-an-array-into-subarrays-with-minimum-cost-i.py
+++ 3263-divide-an-array-into-subarrays-with-minimum-cost-i/divide-an-array-into-subarrays-with-minimum-cost-i.py
@@ -2,18 +2,10 @@ class Solution:
     def minimumCost(self, nums: List[int]) -> int:
         n = len(nums)
 
-        # suffixMin[i] = min(nums[i..n-1])
-        suffixMin = [0] * n
-        suffixMin[-1] = nums[-1]
+        min_sum = nums[0]
+        arr = nums[1:]
+        arr.sort()
+        min_sum += arr[0]
+        min_sum += arr[1]
 
-        for i in range(n - 2, -1, -1):
-            suffixMin[i] = min(nums[i], suffixMin[i + 1])
-
-        ans = float("inf")
-
-        # i is the start of the second subarray
-        for i in range(1, n - 1):
-            cost = nums[0] + nums[i] + suffixMin[i + 1]
-            ans = min(ans, cost)
-
-        return ans
+        return min_sum
