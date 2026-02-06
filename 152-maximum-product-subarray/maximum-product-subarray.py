e502260115b56a39fa969f13b97d993f969d93a6
Muluken Zewge <mulukenzewge@gmail.com>
2026-02-06 11:39:07 +0300

    Time: 2 ms (96.06%) | Memory: 19.8 MB (42.82%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.py
+++ 152-maximum-product-subarray/maximum-product-subarray.py
@@ -4,12 +4,21 @@ class Solution:
         max_prod = 1 # track the max shown so far
         global_max = float("-inf")
 
-        for num in nums:
-            prev_min = min_prod
-            prev_max = max_prod
+        # for num in nums:
+        #     prev_min = min_prod
+        #     prev_max = max_prod
 
-            min_prod = min(num, prev_min*num, prev_max*num)
-            max_prod = max(num, prev_min*num, prev_max*num)
-            global_max = max(global_max, max_prod)
+        #     min_prod = min(num, prev_min*num, prev_max*num)
+        #     max_prod = max(num, prev_min*num, prev_max*num)
+        #     global_max = max(global_max, max_prod)
+        
+        # return global_max
+        for num in nums:
+            if num < 0:
+                min_prod, max_prod = max_prod, min_prod
+            
+            max_prod = max(num, num * max_prod)
+            min_prod = min(num, num * min_prod)
+            global_max = max(global_max, max_prod, min_prod)
         
         return global_max
