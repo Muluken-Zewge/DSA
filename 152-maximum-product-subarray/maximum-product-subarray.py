48a85602a82b1b7486449ff645a8d09818b4e367
Muluken Zewge <mulukenzewge@gmail.com>
2026-02-06 11:05:25 +0300

    Time: 7 ms (63.27%) | Memory: 19.9 MB (25.90%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.py
+++ 152-maximum-product-subarray/maximum-product-subarray.py
@@ -2,14 +2,14 @@ class Solution:
     def maxProduct(self, nums: List[int]) -> int:
         min_prod = 1 # tracks the min showns so far
         max_prod = 1 # track the max shown so far
-        res = float("-inf")
+        global_max = float("-inf")
 
         for num in nums:
-            min_prod0 = min_prod
-            max_prod0 = max_prod
+            prev_min = min_prod
+            prev_max = max_prod
 
-            min_prod = min(num, min_prod0*num, max_prod0*num)
-            max_prod = max(num, min_prod0*num, max_prod0*num)
-            res = max(res, min_prod, max_prod)
+            min_prod = min(num, prev_min*num, prev_max*num)
+            max_prod = max(num, prev_min*num, prev_max*num)
+            global_max = max(global_max, max_prod)
         
-        return res
+        return global_max
