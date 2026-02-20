143a810ddef57336a88147024fe37a5f7dd18d9a
Muluken Zewge <mulukenzewge@gmail.com>
2026-02-20 11:05:09 +0300

    Time: 0 ms (100.00%) | Memory: 21 MB (59.65%) - LeetSync

--- 783-search-in-a-binary-search-tree/search-in-a-binary-search-tree.py
+++ 783-search-in-a-binary-search-tree/search-in-a-binary-search-tree.py
@@ -7,26 +7,27 @@
 class Solution:
     def searchBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
 
-        # def search(node):
-        #     if not node:
-        #         return None
-        #     if node.val == val:
-        #         return node
-        #     if val < node.val:
-        #         return search(node.left)
-        #     if val > node.val:
-        #         return search(node.right)   
-                     
-        # return search(root)
-        stack = [root]
-        while stack:
-            node = stack.pop()
-            if node is None:
+        def search(node):
+            if not node:
                 return None
-            if val == node.val:
+            if node.val == val:
                 return node
             if val < node.val:
-                stack.append(node.left)
+                return search(node.left)
             if val > node.val:
-                stack.append(node.right)
-        
+                return search(node.right)   
+                     
+        return search(root)
+
+        # --- iterative verson ---
+        # stack = [root]
+        # while stack:
+        #     node = stack.pop()
+        #     if node is None:
+        #         return None
+        #     if val == node.val:
+        #         return node
+        #     if val < node.val:
+        #         stack.append(node.left)
+        #     if val > node.val:
+        #         stack.append(node.right)
