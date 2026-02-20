dee4a428bca28819838dd102714338da97e3ff07
Muluken Zewge <mulukenzewge@gmail.com>
2026-02-20 14:10:16 +0300

    Time: 0 ms (100.00%) | Memory: 21.4 MB (14.17%) - LeetSync

--- 784-insert-into-a-binary-search-tree/insert-into-a-binary-search-tree.py
+++ 784-insert-into-a-binary-search-tree/insert-into-a-binary-search-tree.py
@@ -7,14 +7,33 @@
 class Solution:
     def insertIntoBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
 
-        def insert(node,val):
-            if node is None:
-                return TreeNode(val)
-            if val < node.val:
-                node.left = insert(node.left,val)
-                return node
-            if val > node.val:
-                node.right = insert(node.right,val)
-                return node
+        # def insert(node,val):
+        #     if node is None:
+        #         return TreeNode(val)
+        #     if val < node.val:
+        #         node.left = insert(node.left,val)
+        #         return node
+        #     if val > node.val:
+        #         node.right = insert(node.right,val)
+        #         return node
         
-        return insert(root,val)
+        # return insert(root,val)
+
+        if root is None:
+            return TreeNode(val)
+
+        stack = [root]
+        while stack:
+            node = stack.pop()
+            if val < node.val:
+                if node.left is None:
+                    node.left = TreeNode(val)
+                else:
+                    stack.append(node.left)
+            if val > node.val:
+                if node.right is None:
+                    node.right = TreeNode(val)
+                else:
+                    stack.append(node.right)
+        
+        return root
