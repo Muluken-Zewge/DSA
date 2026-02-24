98b92fbefb8a84dfa477d8f37cd51485bf0872ac
Muluken Zewge <mulukenzewge@gmail.com>
2026-02-24 15:26:57 +0300

    Time: 0 ms (100.00%) | Memory: 19.6 MB (25.88%) - LeetSync

--- 1079-sum-of-root-to-leaf-binary-numbers/sum-of-root-to-leaf-binary-numbers.py
+++ 1079-sum-of-root-to-leaf-binary-numbers/sum-of-root-to-leaf-binary-numbers.py
@@ -9,19 +9,40 @@ class Solution:
         # use bit manipulation
         # left shift to create space for the new bit and ORed it with the new bit to add it to the number
 
-        def dfs(node,curr):
-            if not node:
-                return 0
+        # def dfs(node,curr):
+        #     if not node:
+        #         return 0
 
-            # calulate curr value
+        #     # calulate curr value
+        #     curr = (curr << 1) | node.val
+
+        #     # if leaf node -> return completed number
+        #     if not node.left and not node.right:
+        #         return curr
+            
+        #     left = dfs(node.left, curr)
+        #     right = dfs(node.right, curr)
+        #     return left + right
+        
+        # return dfs(root,0)
+
+        # --- iterative version ---
+        total = 0
+        stack = [(root,0)] # (node,curr_binary_val)
+
+        while stack:
+            node, curr = stack.pop()
+
+            # calculate the new curr value
             curr = (curr << 1) | node.val
 
-            # if leaf node -> return completed number
+            # if node is leaf, add to total
             if not node.left and not node.right:
-                return curr
+                total += curr
+            else:
+                if node.left:
+                    stack.append((node.left,curr))
+                if node.right:
+                    stack.append((node.right,curr))
             
-            left = dfs(node.left, curr)
-            right = dfs(node.right, curr)
-            return left + right
-        
-        return dfs(root,0)
+        return total
