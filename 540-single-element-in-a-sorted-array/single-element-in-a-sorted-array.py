b77a98c8823e3febd1eb6c0b1559f68df9280ad3
Muluken Zewge <mulukenzewge@gmail.com>
2026-02-25 13:37:28 +0300

    Time: 0 ms (100.00%) | Memory: 27.2 MB (9.98%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.py
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.py
@@ -1,6 +1,21 @@
 class Solution:
     def singleNonDuplicate(self, nums: List[int]) -> int:
-        res = 0
-        for num in nums:
-            res = res ^ num
-        return res
+        # before the single number occurs, the starting index of the pair is even and the ending index is odd
+        # after the single number, the strting index is odd and the ending is even
+
+        left, right = 0, len(nums) - 1
+        while left < right:
+            mid = left + (right - left) // 2
+            
+            if mid % 2 == 0:
+                if nums[mid] == nums[mid+1]:
+                    left = mid + 1
+                else:
+                    right = mid
+            else:
+                if nums[mid] == nums[mid-1]:
+                    left = mid + 1
+                else:
+                    right = mid
+        
+        return nums[left]
