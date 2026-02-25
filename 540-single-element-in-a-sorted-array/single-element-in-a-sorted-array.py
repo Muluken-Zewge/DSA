2218c640432e9815fa2304a9c1b9d8fb012b8220
Muluken Zewge <mulukenzewge@gmail.com>
2026-02-25 13:42:27 +0300

    Time: 0 ms (100.00%) | Memory: 26.9 MB (73.89%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.py
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.py
@@ -8,14 +8,16 @@ class Solution:
             mid = left + (right - left) // 2
             
             if mid % 2 == 0:
+                # correct pattern(even,odd) -> shring to the right
                 if nums[mid] == nums[mid+1]:
                     left = mid + 1
-                else:
+                else:#(odd,even)->single number exists ->shrink to left
                     right = mid
             else:
+                # correct pattern(even,odd) -> shring to the right
                 if nums[mid] == nums[mid-1]:
                     left = mid + 1
-                else:
+                else:#(odd,even)->single number exists ->shrink to left
                     right = mid
         
         return nums[left]
