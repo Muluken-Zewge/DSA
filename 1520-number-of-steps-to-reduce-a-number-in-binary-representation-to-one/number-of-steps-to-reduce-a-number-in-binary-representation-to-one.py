7359f5cabf9389e191a8f17a9e297e25389c2e4b
Muluken Zewge <mulukenzewge@gmail.com>
2026-02-26 09:41:29 +0300

    Time: 4 ms (19.46%) | Memory: 19.3 MB (80.78%) - LeetSync

--- 1520-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/number-of-steps-to-reduce-a-number-in-binary-representation-to-one.py
+++ 1520-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/number-of-steps-to-reduce-a-number-in-binary-representation-to-one.py
@@ -5,7 +5,7 @@ class Solution:
 
         while num > 1:
             if num % 2 == 0:
-                num //= 2
+                num >>= 1
             else:
                 num += 1
             count += 1
