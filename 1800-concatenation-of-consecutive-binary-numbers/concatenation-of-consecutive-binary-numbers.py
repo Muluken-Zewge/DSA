ccb0448991ceed7d2b26a253b9159f6c0945534e
Muluken Zewge <mulukenzewge@gmail.com>
2026-02-28 11:14:06 +0300

    Time: 755 ms (42.77%) | Memory: 19.1 MB (94.34%) - LeetSync

--- 1800-concatenation-of-consecutive-binary-numbers/concatenation-of-consecutive-binary-numbers.py
+++ 1800-concatenation-of-consecutive-binary-numbers/concatenation-of-consecutive-binary-numbers.py
@@ -1,11 +1,20 @@
 class Solution:
     def concatenatedBinary(self, n: int) -> int:
+        """concatinating a new number means left shifting the current
+        number by the number of bits of the new number(creating a space for
+        the new bits) and ORing with the new number. the way we counted bit
+        number is from the fact that a bit number increases by one when the
+        number is a power of two
+        """
         MOD = 10 ** 9 + 7
-        binary = ''
-        i = 1
+        res = 0
+        length = 0 # current bit length
 
-        while i <= n:
-            binary += bin(i)[2:]
-            i += 1
-
-        return int(binary,2) % MOD
+        for i in range(1,n+1):
+            # increase bit count if i is power of two
+            if i & (i-1) == 0:
+                length += 1
+            
+            res = ((res << length) | i) % MOD
+        
+        return res
