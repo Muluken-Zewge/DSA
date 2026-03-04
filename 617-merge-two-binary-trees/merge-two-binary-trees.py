d24dba79f2a80c7e5826d1292b730e9670f54959
Muluken Zewge <mulukenzewge@gmail.com>
2026-03-04 12:11:39 +0300

    Time: 4 ms (29.87%) | Memory: 19.6 MB (41.76%) - LeetSync

--- 617-merge-two-binary-trees/merge-two-binary-trees.py
+++ 617-merge-two-binary-trees/merge-two-binary-trees.py
@@ -6,32 +6,17 @@
 #         self.right = right
 class Solution:
     def mergeTrees(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> Optional[TreeNode]:
-
+        
         def merge(node1,node2):
-            # base case
-            if node1 is None and node2 is None:
+            if not node1 and not node2:
                 return None
             
-            if node1 is None:
-                new_node = TreeNode(node2.val)
-                left = merge(None,node2.left)
-                right = merge(None,node2.right)
-                new_node.left = left
-                new_node.right = right
-                return new_node
-            elif node2 is None:
-                new_node = TreeNode(node1.val)
-                left = merge(node1.left,None)
-                right = merge(node1.right,None)
-                new_node.left = left
-                new_node.right = right
-                return new_node
-            else: # both are not none
-                new_node = TreeNode(node1.val + node2.val)
-                left = merge(node1.left,node2.left)
-                right = merge(node1.right,node2.right)
-                new_node.left = left
-                new_node.right = right
-                return new_node
+            val = (node1.val if node1 else 0) + (node2.val if node2 else 0)
+            new_node = TreeNode(val)
+
+            new_node.left = merge(node1.left if node1 else None,node2.left if node2 else None)
+            new_node.right = merge(node1.right if node1 else None,node2.right if node2 else None)
+
+            return new_node
         
         return merge(root1,root2)
