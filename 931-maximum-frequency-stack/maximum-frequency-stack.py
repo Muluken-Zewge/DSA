c3a43c938826ac1f258fd5471d0cf2417f0bc820
Muluken Zewge <mulukenzewge@gmail.com>
2026-03-12 12:39:43 +0300

    Time: 78 ms (58.52%) | Memory: 27.9 MB (57.25%) - LeetSync

--- 931-maximum-frequency-stack/maximum-frequency-stack.py
+++ 931-maximum-frequency-stack/maximum-frequency-stack.py
@@ -6,25 +6,26 @@ class FreqStack:
         # store elements with the same count as groups
         self.stacks = defaultdict(list) 
         # number of times most occured val(s) occured
-        self.max_count = 0 
+        self.max_freq = 0 
 
     def push(self, val: int) -> None:
         #update counter
-        self.counter[val] += 1
-        # update max_count
-        if self.counter[val] > self.max_count:
-            self.max_count = self.counter[val]
+        count = self.counter[val] + 1
+        self.counter[val] = count
+        # update max_freq
+        if count > self.max_freq:
+            self.max_freq = count
         # update stacks
-        self.stacks[self.counter[val]].append(val)
+        self.stacks[count].append(val)
 
     def pop(self) -> int:
         # pop the most frequent value
-        poped_val = self.stacks[self.max_count].pop()
+        poped_val = self.stacks[self.max_freq].pop()
         # update counter of popped val
         self.counter[poped_val] -= 1
-        # update max_count
-        if not self.stacks[self.max_count]:
-            self.max_count -= 1
+        # update max_freq
+        if not self.stacks[self.max_freq]:
+            self.max_freq -= 1
         return poped_val
 
 
