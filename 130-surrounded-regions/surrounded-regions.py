a786fb654f3d8a8baf92f0ef1a26579ac936a31f
Muluken Zewge <mulukenzewge@gmail.com>
2026-03-16 09:51:22 +0300

    Time: 6 ms (59.91%) | Memory: 22.3 MB (72.86%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.py
+++ 130-surrounded-regions/surrounded-regions.py
@@ -3,32 +3,36 @@ class Solution:
         """
         Do not return anything, modify board in-place instead.
         """
+
+        """we start from the edge 'O's and mark all their neighbours '#' to
+        mark them back to 'O' since they're guarnteed to be not surrounded.
+        the untouched 'O's in this process are guaranted to be surrounded.
+        """
         n, m = len(board), len(board[0])
-        def dfs(r,c,tracker):
+
+        def dfs(r,c):
             if (r < 0 or r >= n) or (c < 0 or c >= m):
                 return
-            if board[r][c] == "X":
+            if board[r][c] != "O":
                 return
-            if (r == 0 or r == n-1) or (c == 0 or c == m-1):
-                tracker["surrounded"] = False
-            board[r][c] = "X"
-            tracker["indices"].append((r,c))
-
-            dfs(r+1,c,tracker)
-            dfs(r-1,c,tracker)
-            dfs(r,c+1,tracker)
-            dfs(r,c-1,tracker)
-
+            
+            board[r][c] = "#"
+            dfs(r+1,c)
+            dfs(r-1,c)
+            dfs(r,c+1)
+            dfs(r,c-1)
+        
+        # mark boarder-connected 'O's
+        for r in range(n):
+            dfs(r,0) # left border
+            dfs(r,m-1) # right border
+        for c in range(m):
+            dfs(0,c) # top border
+            dfs(n-1,c) # bottom border
+        
         for r in range(n):
             for c in range(m):
-                if board[r][c] == "O":
-                    tracker = {
-                        "surrounded": True,
-                        "indices": []
-                    }
-                    dfs(r,c,tracker)
-                    if tracker["surrounded"] == False:
-                        for i,j in tracker["indices"]:
-                            board[i][j] = "O"
-
-        
+                if board[r][c] == "#":
+                    board[r][c] = "O"
+                elif board[r][c] == "O":
+                    board[r][c] = "X"
