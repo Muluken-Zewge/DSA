bc6d0ac45b2b11a5cc3960e33f54cb6f14232525
Muluken Zewge <mulukenzewge@gmail.com>
2026-03-31 13:22:46 +0300

    Time: 69 ms (31.86%) | Memory: 22.8 MB (55.15%) - LeetSync

--- 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.py
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.py
@@ -8,6 +8,8 @@
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
 
+        # this was my original approach
+        '''
         def findPath(root,node):
             path = []
             curr = root
@@ -31,4 +33,22 @@ class Solution:
             if path_p[i] == path_q[i]:
                 return TreeNode(path_q[i])
             i -= 1
-        
+    '''
+        # this is the best approach
+        '''if both p and q are less than the curr, the LCA is to the left.
+        if both of them are greater than curr, the LCA is to the right.
+        else: we have two cases: 
+        1. p is in the right and q is in the left(or the reverse), then the
+        curr is the BST. it doesn't matter how far p or q are in their
+        subtree.
+        2. p and q are in the same path, the LCA is the one on top(either p
+        or q) which is equal to curr.
+        '''
+        curr = root
+        while curr:
+            if p.val < curr.val and q.val < curr.val:
+                curr = curr.left
+            elif p.val > curr.val and q.val > curr.val:
+                curr = curr.right
+            else:
+                return curr
