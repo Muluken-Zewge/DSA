3d32cd8133a2ffc60f2379360883624eef769949
Muluken Zewge <mulukenzewge@gmail.com>
2026-05-18 17:10:33 +0300

    Time: 39 ms (20.20%) | Memory: 23.6 MB (50.65%) - LeetSync

--- 1016-subarray-sums-divisible-by-k/subarray-sums-divisible-by-k.py
+++ 1016-subarray-sums-divisible-by-k/subarray-sums-divisible-by-k.py
@@ -1,21 +1,18 @@
 class Solution:
     def subarraysDivByK(self, nums: List[int], k: int) -> int:
-        """ this is also a prefix difference pattern just like 560.
-        this time, we track the remainder and if a prefix sum equal to 
-        the  remainder exists,that means removing that prefix would make
-        the current prefix remainder a zero->sub array between the two
-        indices is divisible by k. 
-        """
-
-        curr_sum = 0
-        ans = 0
-        prefix_map = defaultdict(int) 
-        prefix_map[0] = 1 # base case: for subarrays starting from index 0
+        '''If two prefix sums leave the same remainder when divided by k,
+        then the subarray between them is divisible by k.
+        '''
+        prefix_state = {0:1} # {prefix_sum:frequency}
+        count = 0
+        prefix_sum = 0
 
         for n in nums:
-            curr_sum += n
-            remain = curr_sum % k
-            ans += prefix_map[remain]
-            prefix_map[remain] += 1
-        
-        return ans
+            prefix_sum += n
+            mod = prefix_sum % k
+            if mod in prefix_state:
+                count += prefix_state[mod]
+            
+            prefix_state[mod] = prefix_state.get(mod,0) + 1
+           
+        return count
