6027893581bfd3072d00608dbb664d270d8b9e07
Muluken Zewge <mulukenzewge@gmail.com>
2026-06-02 14:10:10 +0300

    Time: 8 ms (79.07%) | Memory: 19.4 MB (18.02%) - LeetSync

--- 3965-earliest-finish-time-for-land-and-water-rides-i/earliest-finish-time-for-land-and-water-rides-i.py
+++ 3965-earliest-finish-time-for-land-and-water-rides-i/earliest-finish-time-for-land-and-water-rides-i.py
@@ -3,23 +3,23 @@ class Solution:
         n , m = len(landStartTime), len(waterStartTime)
         ans = float("inf")
         # find earliest finish time from land
-        early_land = 10 ** 4
+        earliest_land_finish = 10 ** 4
         for i in range(n):
-            early_land = min(early_land, landStartTime[i]+landDuration[i])
+            earliest_land_finish = min(earliest_land_finish, landStartTime[i]+landDuration[i])
+        
+         # find earliest finish time from water
+        earliest_water_finish = 10 ** 4
+        for i in range(m):
+            earliest_water_finish = min(earliest_water_finish, waterStartTime[i]+waterDuration[i])
 
         # find total early finish starting from land
         for i in range(m):
-            curr_ans = max(early_land,waterStartTime[i]) + waterDuration[i]
+            curr_ans = max(earliest_land_finish,waterStartTime[i]) + waterDuration[i]
             ans = min(ans, curr_ans)
-        
-        # find earliest finish time from water
-        early_water = 10 ** 4
-        for i in range(m):
-            early_water = min(early_water, waterStartTime[i]+waterDuration[i])
         
         # find total early finish starting from water
         for i in range(n):
-            curr_ans = max(early_water,landStartTime[i]) + landDuration[i]
+            curr_ans = max(earliest_water_finish,landStartTime[i]) + landDuration[i]
             ans = min(ans, curr_ans)
         
         return ans
