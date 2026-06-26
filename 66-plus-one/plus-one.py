0a90d700338349ca00bab35b4b852db12923dde3
Muluken Zewge <mulukenzewge@gmail.com>
2026-06-26 12:31:19 +0300

    Time: 0 ms (100.00%) | Memory: 19.4 MB (18.59%) - LeetSync

--- 66-plus-one/plus-one.py
+++ 66-plus-one/plus-one.py
@@ -1,16 +1,30 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
         # pure math approach
-        n = len(digits)
-        num = 0
-        base = 0
-        for i in range(n-1,-1,-1):
-            num += digits[i] * 10 ** base
-            base += 1
-        num += 1
+        # n = len(digits)
+        # num = 0
+        # base = 0
+        # for i in range(n-1,-1,-1):
+        #     num += digits[i] * 10 ** base
+        #     base += 1
+        # num += 1
+        # ans = []
+        # while num > 0:
+        #     ans.append(num%10)
+        #     num //= 10
+        
+        # return ans[::-1]
+
+        # -- dsa approach --
+        rem = 1
         ans = []
-        while num > 0:
-            ans.append(num%10)
-            num //= 10
+        for i in range(len(digits)-1,-1,-1):
+            if digits[i] + rem == 10:
+                ans.append(0)
+            else:
+                ans.append(digits[i]+rem)
+                rem = 0
+        if rem == 1:
+            ans.append(rem)
         
         return ans[::-1]
