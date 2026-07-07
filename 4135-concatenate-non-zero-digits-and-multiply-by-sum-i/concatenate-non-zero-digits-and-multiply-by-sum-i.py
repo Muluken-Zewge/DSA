4614459bab78572e8527f7d75a227a94dd46ef78
Muluken Zewge <mulukenzewge@gmail.com>
2026-07-07 14:05:32 +0300

    Time: 5 ms (1.39%) | Memory: 19.4 MB (14.68%) - LeetSync

--- 4135-concatenate-non-zero-digits-and-multiply-by-sum-i/concatenate-non-zero-digits-and-multiply-by-sum-i.py
+++ 4135-concatenate-non-zero-digits-and-multiply-by-sum-i/concatenate-non-zero-digits-and-multiply-by-sum-i.py
@@ -1,15 +1,14 @@
 class Solution:
     def sumAndMultiply(self, n: int) -> int:
-        num_arr = []
         num = 0
         power = 0
+        sum_ = 0
         while n > 0:
             mod = n % 10
             if mod != 0:
-                num_arr.append(mod)
+                sum_ += mod
                 num += mod * (10**power)
                 power += 1
             n //= 10
-        sum_ = sum(num_arr)
 
         return num*sum_ 
