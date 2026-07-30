b5d00fa88a25a0cad6d2b6ccd35f1381c33584db
Muluken Zewge <mulukenzewge@gmail.com>
2026-07-30 16:34:30 +0300

    Time: 79 ms (12.72%) | Memory: 19.8 MB (69.08%) - LeetSync

--- 2793-count-the-number-of-complete-components/count-the-number-of-complete-components.py
+++ 2793-count-the-number-of-complete-components/count-the-number-of-complete-components.py
@@ -6,28 +6,53 @@ class Solution:
             graph[b].append(a)
 
         visited = set()
-        def dfs(node):
-            if node in visited:
-                return 0,0
+        # def dfs(node):
+        #     if node in visited:
+        #         return 0,0
 
-            visited.add(node)
+        #     visited.add(node)
 
-            nodes = 1
-            edge_count = len(graph[node])
-            for child in graph[node]:
-                child_nodes, child_edges = dfs(child)
-                nodes += child_nodes
-                edge_count += child_edges
+        #     nodes = 1
+        #     edge_count = len(graph[node])
+        #     for child in graph[node]:
+        #         child_nodes, child_edges = dfs(child)
+        #         nodes += child_nodes
+        #         edge_count += child_edges
             
-            return nodes,edge_count
+        #     return nodes,edge_count
 
+        # ans = 0
+        # for node in range(n):
+        #     if node not in visited:
+        #         nodes, edges = dfs(node)
+        #         edges //= 2 # edges were counted twice
+        #         if edges == nodes*(nodes - 1)//2:
+        #             ans += 1
+        
+        # return ans
+        print(graph)
         ans = 0
-        for node in range(n):
-            if node not in visited:
-                nodes, edges = dfs(node)
-                edges //= 2 # edges were counted twice
-                if edges == nodes*(nodes - 1)//2:
-                    ans += 1
+        for i in range(n):
+            if i in visited:
+                continue
+
+            queue = deque([i])
+            visited.add(i)
+            nodes, edge_count = 0, 0
+
+            while queue:
+                node = queue.popleft()
+                nodes += 1
+                edge_count += len(graph[node])
+
+                for child in graph[node]:
+                    if child not in visited:
+                        queue.append(child)
+                        visited.add(child)
+            
+            if edge_count//2 == nodes*(nodes - 1)//2:
+                ans += 1
         
         return ans
+            
         
